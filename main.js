a4b0a5782cbf77316572a091e5411cbf5ab342f9
Tasmiah-coder <tasmiahtabassum13218@gmail.com>
2024-10-07 10:56:56 +0600

    solve problem 1,2

---
+++ main.js
@@ -0,0 +1,21 @@
+// Math problem-1 solution 
+
+let numberOfRemainingApple = 25;
+let numberOfAppleBought = 15;
+
+let totalApple = numberOfRemainingApple + numberOfAppleBought;
+
+console.log('Number of Total Apple is: '+ totalApple)
+
+
+
+
+// Math problem-2 solution 
+
+let capitalAmmount = 500;
+let JohnHasSpent = 200;
+
+let remainingAmmount = capitalAmmount - JohnHasSpent;
+
+console.log('The remaining ammount Jhon has TK. ' + remainingAmmount)
+
